400e2b9617dd696e962586c35c9f64181c9b60a6
kamyarkamali <kamyarkamali021@gmail.com>
2024-11-25 17:22:03 +0330

    end project

--- app/components/module/CardPopular.js
+++ app/components/module/CardPopular.js
@@ -17,7 +17,8 @@ function CardPopular() {
   return (
     <div>
       <Swiper
-        spaceBetween={20} // فاصله بین اسلایدها
+        centeredSlides={true}
+        spaceBetween={30}
         breakpoints={{
           320: {
             slidesPerView: 1.2,
@@ -33,13 +34,12 @@ function CardPopular() {
         {filtereddata.map((item, index) => (
           <SwiperSlide key={index}>
             <div className="w-[290px] cursor-pointer h-[296px] relative flex flex-col gap-4 p-4">
-              {/* فاصله بین تصاویر با gap */}
               <img
                 className="w-[290px] h-[290px] object-cover "
                 src={item.image}
                 alt={`Popular item ${index + 1}`}
               />
-              <div className="w-[249px] h-[68px] backdrop-blur-lg border-[1px] flex justify-center items-center absolute bottom-[40px]">
+              <div className="w-[249px] h-[68px] backdrop-blur-lg border-[1px] flex justify-center items-center absolute bottom-[40px] left-1/2 transform -translate-x-1/2">
                 <div className="flex justify-between w-full">
                   <div className="flex flex-col mr-2">
                     <div className="flex items-center gap-1">
@@ -51,11 +51,21 @@ function CardPopular() {
                     </div>
                   </div>
                   <div className="flex flex-col justify-center ">
-                    <div className="ml-2 flex flex-col items-center mb-3">
+                    <div
+                      className={`ml-2 flex flex-col items-center mb-3 ${
+                        Object.values(item)[0] ? "ml-[6px]" : ""
+                      }`}
+                    >
                       <p className="text-[12px] font-bold">{item.title}</p>
                       <div className="flex items-center justify-center">
                         <p className="text-[12px] text-ce">{item.title2}</p>
-                        <img className="mr-[1px]" src={item.image2} alt="/" />
+                        <img
+                          className={`${
+                            Object.values(item)[0] ? "ml-[3px]" : ""
+                          }`}
+                          src={item.image2}
+                          alt="/"
+                        />
                       </div>
                     </div>
                   </div>
